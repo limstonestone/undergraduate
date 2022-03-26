9995d3d50b737b6d02d2521fcf1ec3875ad3c399
limstonestone <86715604+limstonestone@users.noreply.github.com>
2022-03-26 11:55:13 +0900

    Add files

--- 3학년 1학기/Introduction_To_Biostatistics/Lecture02_Statistical Tests for Genomic Data.R
+++ 3학년 1학기/Introduction_To_Biostatistics/Lecture02_Statistical Tests for Genomic Data.R
@@ -128,3 +128,100 @@ t.test(golub[ccnd3, ALL], golub[ccnd3, !ALL], var.equal=FALSE,
 ## Pooled two-sample t-test (var.equal = TRUE)
 ## 등분산 검정 우선되어야 함
 t.test(golub[ccnd3,] ~ golubFactor, var.equal=TRUE)
+
+# Permutation Test
+## 현실 데이터는 대개 분포가 알려져 있지 않고 sample size 가 제한되어 있기 때문에 사용
+## 단점으로는 시간이 많이 걸림
+## resampling-based test
+golubFactor
+sample(golubFactor)
+sample(as.numeric(golubFactor))
+
+y = as.numeric(golubFactor)
+K = 1000
+mat.y = matrix(y, length(y), K)
+
+per.y = apply(mat.y, 2, sample)
+t.test(golub[ccnd3, ] ~ per.y[,1], var.equal=FALSE)
+t.test(golub[ccnd3, ] ~ per.y[,1], var.equal=FALSE)$stat  # to get test-statistics
+
+fun = function(t) t.test(golub[ccnd3,]~t, var.equal=FALSE)$stat
+p.stat = apply(per.y, 2, fun)
+hist(p.stat, nclass=50, col="orange") # looks like Normal Dist
+
+tobs = t.test(golub[ccnd3,]~golubFactor, var.equal=FALSE)$stat
+mean(abs(p.stat) > abs(tobs)) # p-value
+(sum(abs(p.stat) > abs(tobs)) + 1)/(K+1)  # significantly reject H0
+
+gdf5 = grep("Gdf5", golub.gnames[,2], ignore.case=TRUE)
+fun2 = function(t) t.test(golub[gdf5,]~t, var.equal=FALSE)$stat
+p.stat2 = apply(per.y, 2, fun2)
+hist(p.stat2, nclass=50, col="orange")
+
+tobs2 = t.test(golub[gdf5,]~golubFactor, var.equal=FALSE)$stat
+abline(v=c(-tobs2, tobs2), col="red", lty=2, lwd=2)
+
+mean(abs(p.stat2) > abs(tobs2))
+(sum(abs(p.stat2) > abs(tobs2))+1)/(K+1)  # not reject H0
+
+# Test for Equal Variances
+## t-test 의 가정에 두 모집단의 등분산성을 가정
+## 등분산성을 어떻게 검정?
+var.test(golub[ccnd3, ] ~ golubFactor)  # not reject H0
+var.test(golub[gdf5, ] ~ golubFactor)   # "pooled t-test" 사용 가능
+## F-test는 정규성에 민감하므로 대체로 다음 test도 많이 사용
+## The Bartlett test
+## The Fligner-Killeen (median) test
+## The Levenee test
+bartlett.test(golub[ccnd3, ] ~ golubFactor)
+bartlett.test(golub[gdf5, ] ~ golubFactor)
+
+fligner.test(golub[ccnd3,], golubFactor)
+fligner.test(golub[gdf5,], golubFactor)
+
+install.packages("car")
+library(car)
+
+leveneTest(golub[ccnd3,], golubFactor)
+leveneTest(golub[gdf5,], golubFactor)
+
+# Binomial Test
+## descrete distribution 이므로 등호 주의
+## Example : microRNA of length 22 contains 18 purines
+## H0 : p=0.7
+sum(dbinom(18:22, 22, 0.7)) # P(X>=18)
+1 - pbinom(17, 22, 0.7)     # 1 - P(X<17)
+
+binom.test(18, 22, p=0.7, alternative="greater", conf.level=0.95)
+
+# Chi-squared Test
+## 하나 이상의 특정 확률을 따르냐에 대한 검정
+## Example : Let the probability H0 : (1/4, 1/4, 1/4, 1/4)
+install.packages("ape")
+library(ape)
+
+zyxinfreq = table(read.GenBank(c("X94991.1"), as.character=TRUE))
+zyxinfreq
+chisq.test(zyxinfreq) # significantly reject H0 -> probabilities are very different
+
+f = function(x) dchisq(x, 3)
+plot(f, 0, 25, cex.lab=1.5, xlab="q", ylab="f(q)",
+     main="Chi-squared probability density function f(q)")
+
+alpha = 0.05
+x1 = seq(0, qchisq(1-alpha, 3), 0.01)
+x2 = seq(qchisq(1-alpha, 3), 20, 0.01)
+
+polygon(c(0,x1,qchisq(1-alpha,3)),c(0,f(x1),0),col="lightblue")
+polygon(c(qchisq(1-alpha,3),x2,20), c(0,f(x2),0), col="red")
+
+arrows(10,0.045,10,0.02)
+text(10,0.06,"Rejection")
+text(10,0.05,"Region")
+text(3,0.04,"Acceptance")
+text(3,0.03,"Region")
+mtext(expression(chi[3]^2),side=1,at=qchisq(1-alpha,3))
+
+pi = c(0.75, 0.25)
+x = c(5474, 1850)
+chisq.test(x, p=pi) # not reject H0 => H0 : (pi1, pi2) = (0.75, 0.25) is True
