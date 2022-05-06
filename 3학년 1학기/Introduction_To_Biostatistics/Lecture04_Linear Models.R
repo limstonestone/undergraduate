82cf52faf794dedf0d3ff9444143f2b3afbfa275
limstonestone <86715604+limstonestone@users.noreply.github.com>
2022-05-06 13:37:09 +0900

    Add files

--- 3학년 1학기/Introduction_To_Biostatistics/Lecture04_Linear Models.R
+++ 3학년 1학기/Introduction_To_Biostatistics/Lecture04_Linear Models.R
@@ -259,3 +259,120 @@ leveneTest(y, factor)
 fit = lm(y ~ factor)$fit
 par(mfrow=c(1,1))
 plot(fit, res, pch=19, col="red", xlab="Fitted values", ylab="Residuals")
+
+# Robust Tests
+## When homoscedasticity is violated -> 'Welch's ANOVA'
+## When normality is violated -> 'Kruskal-Wallis rank sum test'
+
+## Example (Unequal variances)
+oneway.test(y ~ factor, var.equal=FALSE)
+
+oneway.test(y ~ factor, var.equal=TRUE)
+anova(lm(y ~ factor))
+summary(aov(lm(y ~ factor)))
+
+## Example (Non-normality)
+kruskal.test(y ~ factor)
+
+f2 = function(t) kruskal.test(t ~ factor)$p.value
+kruskal.pValues = apply(ex[, B1B2B3], 1, f2)
+
+pBF = p.adjust(kruskal.pValues, method="bonferroni")
+pH0 = p.adjust(kruskal.pValues, method="holm")
+pBH = p.adjust(kruskal.pValues, method="BH")
+
+alpha = 0.05
+c(sum(pBF < alpha), sum(pH0 < alpha), sum(pBH < alpha))
+
+# Two-way Analysis of Variance
+## one-way : only one factor
+## two-way : two(multiple) factors, and interaction effect is exist
+## interaction effect is significant -> effect of one factor depends on the other factor
+
+## Example Two-way ANOVA
+library(ALL)
+data(ALL)
+
+ex = exprs(ALL)
+dim(ex)
+
+table(ALL$BT)
+table(ALL$mol.biol)
+
+w1 = ALL$BT %in% c("B", "B1", "B2", "B3", "B4")
+w2 = ALL$mol.biol %in% c("BCR/ABL", "NEG")
+ex12 = ex["32069_at", w1 & w2]
+length(ex12)
+
+facB = ceiling(as.integer(ALL$BT[w1 & w2])/3)
+facB
+data.frame(B.type=ALL$BT[w1 & w2], facB=facB)
+
+fac1 = factor(facB, levels=1:2, labels=c("B012", "B34"))
+fac2 = factor(ALL$mol.biol[w1 & w2])
+table(fac1)
+table(fac2)
+
+tapply(ex12, fac1, mean)
+tapply(ex12, fac2, mean)
+
+col = c("orange", "blue")
+xlab1 = "B-cell ALL stage"
+xlab2 = "Molecular biology"
+ylab = "NEDD4 expression"
+
+par(mfrow=c(1, 2))
+stripchart(ex12 ~ fac1, method="jitter", cex.lab=1.5,
+           vertical=TRUE, xlab=xlab1, ylab=ylab, col=col)
+stripchart(ex12 ~ fac2, method="jitter", cex.lab=1.5,
+           vertical=TRUE, xlab=xlab2, ylab=ylab, col=col)
+
+boxplot(ex12 ~ fac1, cex.lab=1.5, main=NULL, boxwex=0.3,
+        xlab=xlab1, ylab=ylab, col=col)
+boxplot(ex12 ~ fac2, cex.lab=1.5, main=NULL, boxwex=0.3,
+        xlab=xlab2, ylab=ylab, col=col)
+
+interaction.plot(fac1, fac2, ex12, type="b", col=col, xlab=xlab1,
+                 pch=c(16, 17), lty=1, lwd=2, legend=F, ylab=ylab)
+legend("topright", c("BCR/ABL","NEG"), bty="n", lty=1, lwd=2,
+       pch=c(16,17), col=col, inset=0.02)
+
+
+interaction.plot(fac2, fac1, ex12, type="b", col=col, xlab=xlab2,
+                 pch=c(16, 17), lty=1, lwd=2, legend=F, ylab=ylab)
+legend("topright", c("B012","B34"), bty="n", lty=1, lwd=2,
+       pch=c(16,17), col=col, inset=0.02)
+
+tapply(ex12[fac2=="BCR/ABL"], fac1[fac2=="BCR/ABL"], mean)
+tapply(ex12[fac2=="NEG"], fac1[fac2=="NEG"], mean)
+
+tapply(ex12[fac1=="B012"], fac2[fac1=="B012"], mean)
+tapply(ex12[fac1=="B34"], fac2[fac1=="B34"], mean)
+
+anova(lm(ex12 ~ fac1 * fac2)) # there are two significant main effects as well as a significant interaction effect
+summary(lm(ex12 ~ fac1 * fac2))
+
+# Two-way ANOVA for Multiple Genes
+## In bioinformatics, computing the number of probes with significant main as well as significant interaction effects is important
+w1 = ALL$BT %in% c("B", "B1", "B2", "B3", "B4")
+w2 = ALL$mol.biol %in% c("BCR/ABL", "NEG")
+
+ex = exprs(ALL)
+exw = ex[, w1 & w2]
+dim(exw)
+
+f1 = function(t) anova(lm(t ~ fac1 * fac2))$Pr[1:3]
+anova.pValues = apply(exw, 1, f1)
+
+anova.pValues.t = data.frame(t(anova.pValues))
+cname = c("mainEffect1", "mainEffect2", "interaction")
+colnames(anova.pValues.t) = cname
+anova.pValues.t[1:10, ]
+
+alpha = 0.05
+c1 = anova.pValues.t$mainEffect1 < alpha
+c2 = anova.pValues.t$mainEffect2 < alpha
+c3 = anova.pValues.t$interaction < alpha
+sum(c1 & c2 & c3)
+
+alpha = 0.05/nrow(anova.pValues.t)
